75ef322a5931d4505ab0f2c9b147d93a4b208ee3
Nicholas Riley <com-github@sabi.net>
2016-09-22 20:16:39 -0400

    Rename extension; return false so event can be detected.

--- OpenInNewTab.safariextension/injected.js
+++ OpenInNewTab.safariextension/injected.js
@@ -1,8 +1,9 @@
 window.addEventListener('openInNewTab', function(e) {
+    // Can use return value of dispatchEvent to determine whether extension is installed
+    e.returnValue = false;
     var message = {
         href: e.target.href,
         background: e.detail.background
     };
     safari.self.tab.dispatchMessage('openInNewTab', message);
-    e.preventDefault();
 });
