5f8a611b1b70c8dd6d397f1f86e8c939669807f7
Nicholas Riley <com-github@sabi.net>
2016-09-23 23:05:31 -0400

    Only work from newsblur.com.

--- NewsBlurOpenInNewTab.safariextension/injected.js
+++ NewsBlurOpenInNewTab.safariextension/injected.js
@@ -1,4 +1,8 @@
 window.addEventListener('openInNewTab', function(e) {
+    // only work from newsblur.com
+    if (!e.srcElement.ownerDocument.domain.toLowerCase().endsWith('.newsblur.com'))
+        return;
+
     // Can use return value of dispatchEvent to determine whether extension is installed
     e.returnValue = false;
     var message = {
